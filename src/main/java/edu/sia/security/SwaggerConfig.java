ce93059898c653591270b54d671a47856f01aacc
Santiago Zapata Barahona <santiago.zapatab@udea.edu.co>
2025-10-28 21:04:08 -0500

    feat: Agregar OpenAPI mediante Swagger, se accede buscando http://localhost:8080/swagger-ui/index.html en el navegador

---
+++ src/main/java/edu/sia/security/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package edu.sia.security;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API - Sistema de información académica (SIA)")
+                        .description("Documentación de la API REST para el sistema el sia.")
+                        .version("1.0.0"));
+    }
+}
